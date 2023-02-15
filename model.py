98cb226ffc1201df63187a17c878b2ceb14f3566
jkrpte <125425656+jkrpte@users.noreply.github.com>
2023-02-15 04:03:44 +0000

    Add files via upload
    Written by Dr Rea Laila Antoniou Kourounioti

---
+++ model.py
@@ -0,0 +1,161 @@
+import numpy as np
+import parameters as p
+import math
+
+def model(y,time,tiTi):
+
+	# variables
+	b=y[0]
+	v=y[1]
+	V=y[2]
+	Hs=y[3]
+	Is=y[4]
+	Ns=y[5]
+	Ss=y[6]
+	Ps=y[7]
+	#dividing cells
+	H=y[8]
+	I=y[9]
+	N=y[10]
+	S=y[11]
+	P=y[12]
+	#nondividing cells
+	n=Hs+Is+Ns+Ss+Ps+H+I+N+S+P;
+	#total cells
+	r=y[13]
+	#reactivation 
+	FLC=y[14]
+	#FLC not divided by intial condition
+
+
+
+	# light
+	if len(p.sunrise)>1:
+		if time>p.lightON/24:
+			if p.sunrise[math.floor(time-p.lightON/24),0]==math.floor(time-p.lightON/24):
+				day=p.sunrise[math.floor(time-p.lightON/24),]
+			else:
+				print("sunrise position and day don't match")
+				print("day "+str(p.sunrise[(p.sunrise[:,0]==math.floor(time-p.lightON/24))])+", time "+str(time))
+				day=p.sunrise[(p.sunrise[:,0]==math.floor(time-p.lightON/24))][0]
+		else:
+			if p.sunrise[math.floor(time),0]==math.floor(time):
+				day=p.sunrise[math.floor(time),]
+			else:
+				print("sunrise position and day don't match")
+				print("day "+str(p.sunrise[(p.sunrise[:,0]==math.floor(time))])+", time "+str(time))
+				day=p.sunrise[(p.sunrise[:,0]==math.floor(time))][0]
+	else:
+		day=p.sunrise[0]
+		
+	if day.size==0:
+		print("problem extracting sunrise at time:")
+		print(time)
+		return -1
+	
+	p.lightON=day[1]
+	p.lightOFF=day[2]
+	p.moonSt=day[2]
+
+	
+	# temperature
+	T=np.interp(time,tiTi[0],tiTi[1])
+	nT=nightTime12h(time,p.lightON,p.lightOFF,tiTi)
+
+	# parameters
+	a=(1+(-1+p.A1)*(MAXbefore(tiTi,time,p.moonSt)>p.warm))
+	c=p.C1-((T-p.CT1)/(p.CT2-p.CT1)*(T<p.CT2)*(T>p.CT1)+(T>=p.CT2))*p.C2;
+	d=(p.D1+math.sin(2*math.pi*(time-(p.lightON-1)/24)))**2;
+	pv=a*b*c*d;
+	s1=p.s1
+	s2=p.s2*(p.Tq2-T)*(T-p.Tq1)*(T>p.Tq1)*(T<p.Tq2)*V
+	g=r*p.g1*math.exp(p.growthe*(T-22))
+	dn=p.dn
+	if (FLC<p.kFLC)&(T>p.Tq2):
+		k=p.k
+	else:
+		k=0
+	
+	r1=p.r1*((nT-p.nT1)/(p.nT2-p.nT1)*(nT<p.nT2)*(nT>p.nT1)+(nT>=p.nT2))
+	s3=p.s3*g
+	r2=p.r2*g
+
+	## ODEs
+	out=[(T<p.BT)-p.dB*b,#dB
+		pv-p.sv*v-p.dv*v,#v
+		p.sv*v-p.dV*V,#V
+		-(s1+s2*p.sel)*Hs+r1*Is,#Hs
+		s1*Hs-(r1+s2)*Is+r2*Ps,#Is
+		s2*p.sel*Hs+s2*Is-g*Ns,#Ns
+		g*Ns-s3*Ss,#Ss
+		s3*Ss-r2*Ps,#Ps
+		dn*g*Hs-(s1+s2*p.sel)*H+r1*I,#H
+		dn*r2*Ps+dn*g*Is+s1*H-(r1+s2)*I,#I
+		s2*p.sel*H+s2*I,#N
+		dn*g*Ns+dn*(g-s3)*Ss,#S
+		dn*s3*Ss+dn*(g-r2)*Ps,#P
+		-k*r,#r
+		p.f*(Hs+H)/n - p.l*FLC]#FLC rate of change, deg before FLC 
+		
+	p.tEnd=time
+	return out
+	
+
+
+def output(y):
+	FLCexp=y[:,14]/p.COLFRInv #divides by initial value
+	VIN3=y[:,2]
+	return VIN3,FLCexp
+#adjusted to intial condition of FLC in COLFRI
+
+
+def nightTime12h(time,lightON,lightOFF,tiTi):
+	midday=np.mean([lightON,lightOFF])
+	morningTime=0.25+(midday-12)/24
+	night=math.floor(time+1-morningTime)-1-0.5+morningTime
+	if night<tiTi[0][0]:
+		nT=22
+	else:
+		morning=math.floor(time+1-morningTime)-1+morningTime
+		Tnight=np.interp(night,tiTi[0],tiTi[1])
+		Tmorning=np.interp(morning,tiTi[0],tiTi[1])
+		nightIn=np.searchsorted(tiTi[0],night,side='right')#where(tiTi[0]>night)[0][0]
+		morningIn=np.searchsorted(tiTi[0],morning)-1#where(tiTi[0]<morning)[0][len(np.where(tiTi[0]<morning)[0])-1]
+		if nightIn<morningIn:
+			Tsum=np.array(tiTi[1][nightIn:(morningIn+1)]).cumsum()
+			Tsum[2:]=Tsum[2:]-Tsum[:-2]
+			nT=((Tnight+tiTi[1][nightIn])*(tiTi[0][nightIn]-night)/2+np.dot(Tsum[1:],np.diff(tiTi[0][nightIn:(morningIn+1)]))/2+(Tmorning+tiTi[1][morningIn])*(morning-tiTi[0][morningIn])/2)*2
+		else:
+			if morningIn==nightIn:
+				nT=((Tnight+tiTi[1][nightIn])*(tiTi[0][nightIn]-night)/2+(Tmorning+tiTi[1][morningIn])*(morning-tiTi[0][morningIn])/2)*2
+			else:
+				nT=(Tmorning+Tnight)/2
+	return nT
+
+
+	
+def MAXbefore(tiTi,t,moonEnd):
+	day=math.floor(t)
+	if day<tiTi[0][0]:
+		maxBefore=22
+	else:
+		if t%1<=(moonEnd/24+10**(-10)):
+			if day>0:
+				thatDay=tiTi[1][(tiTi[0]>(day-1+moonEnd/24))&(tiTi[0]<=t)]
+				if len(thatDay)>0:
+					maxBefore=max(np.amax(np.interp([day-1+moonEnd/24,t],tiTi[0],tiTi[1])),np.amax(thatDay))
+				else:
+					maxBefore=np.amax(np.interp([day-1+moonEnd/24,t],tiTi[0],tiTi[1]))
+			else:
+				thatDay=tiTi[1][(np.array([math.floor(x) for x in tiTi[0]])==(day))&(tiTi[0]<=t)]
+				if len(thatDay)>0:
+					maxBefore=max(np.amax(np.interp([day,t],tiTi[0],tiTi[1])),np.amax(thatDay))
+				else:
+					maxBefore=np.amax(np.interp([day,t],tiTi[0],tiTi[1]))
+		else:
+			thatDay=tiTi[1][(tiTi[0]>(day+moonEnd/24))&(tiTi[0]<=t)]
+			if len(thatDay)>0:
+				maxBefore=max(np.amax(np.interp([day+moonEnd/24,t],tiTi[0],tiTi[1])),np.amax(thatDay))
+			else:
+				maxBefore=np.amax(np.interp([day+moonEnd/24,t],tiTi[0],tiTi[1]))
+	return maxBefore
---
+++ parameters.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+import csv
+
+# parameters for 
+s1=			0.016 #ps1
+s2=			0.0111
+s3=			0.75
+sel=		0.1
+r1=			0.05 #pr1
+r2=			0 #pr2
+k=			0.18
+n0i=		0.2 #proportion of silenced cells initially
+Tq1=		-1
+Tq2=		18
+nT1=		11.5
+nT2=		15
+kFLC=       0.04
+
+# constant parameters
+dn=			32
+g1=         0.4
+growthe=	0.22
+f=  		2.77 #pf
+l=          2.77
+# VIN3 parameters
+vV=4
+warm=15
+dV=18
+dv=0
+A1=0.75
+BT=17
+dB=0.009
+CT1=8
+CT2=15.4
+C1=0.0315
+C2=0.03
+D1=2.05
+sv=vV*dV
+
+# related to light and time of day
+lightON=10
+lightOFF=18
+moonSt=16
+moonEnd=18
+sunrise=np.array([[0,lightON,lightOFF]])
+sunrise=sunrise.astype(float)
+
+# initial conditions
+tEnd=0
+wi=1;# vi=[0, 0];
+COLFRInv=(1-n0i)*r1/(s1+r1)
+n1i=(1-n0i)*s1/(s1+r1)
+init=[];#to be defined later
+
+# example parameters and temperature conditions
+tiTi=np.array([[-5,-0.01,0,28,28.01,100],[22,22,5,5,22,22]])
+param=[s1,s2,s3,sel,r1,r2,k,n0i,Tq1,Tq2,nT1,nT2,kFLC, f]
+
+print("%%%%%%%%%%%%%%%%%parameters imported successfully%%%%%%%%%%%%%%")
